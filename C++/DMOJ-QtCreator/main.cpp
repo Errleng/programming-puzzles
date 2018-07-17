43c7474e28abf1f7444c5947b2f17bc932598789
ELEVATE <dullbland@gmail.com>
2018-07-16 23:07:41 -0400

    Project with stack and intellij-haskell

--- C++/DMOJ-QtCreator/main.cpp
+++ C++/DMOJ-QtCreator/main.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 
+// clang-format off
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 char _;
+// clang-format on
 
 #define FOR(i, a) for (int i = 0; i < (a); ++i)
 #define FORN(i, a, b) for (int i = (a); i < (b); ++i)
@@ -11,73 +13,63 @@ using namespace std::chrono;
 
 typedef long long ll;
 typedef unsigned long long ull;
-
-void print() {
-    cout << endl;
-}
-
+void print() { cout << endl; }
 template <typename T, typename... Args>
-void print(T curArg, Args&&... remArgs) {
-    cout << curArg << " ";
-    print(remArgs...) ;
+void print(T curArg, Args &&... remArgs) {
+  cout << curArg << " ";
+  print(remArgs...);
 }
 
-template <typename T, size_t N>
-void printL(T(&arr1D)[N]) {
-    for (size_t elem = 0; elem < N; ++elem) {
-        cout << arr1D[elem] << " ";
+template <typename T, size_t N> void printL(T (&arr1D)[N]) {
+  for (size_t elem = 0; elem < N; ++elem) {
+    cout << arr1D[elem] << " ";
+  }
+  cout << endl;
+}
+
+template <typename T> void printA(T *arr, size_t N) {
+  for (size_t elem = 0; elem < N; ++elem) {
+    cout << arr[elem] << " ";
+  }
+  cout << endl;
+}
+
+template <typename T, size_t N, size_t M> void print2DL(T (&arr2D)[N][M]) {
+  for (size_t r = 0; r < N; ++r) {
+    for (size_t c = 0; c < M; ++c) {
+      cout << arr2D[r][c] << " ";
     }
-    cout << endl;
+    cout << '\n';
+  }
+  cout << endl;
 }
 
-template <typename T>
-void printA(T *arr, size_t N) {
-    for (size_t elem = 0; elem < N; ++elem) {
-        cout << arr[elem] << " ";
+template <typename T> void print2DA(T **arrSqr, size_t N, size_t M) {
+  for (size_t row = 0; row < N; ++row) {
+    for (size_t column = 0; column < M; ++column) {
+      cout << arrSqr[row][column] << " ";
     }
-    cout << endl;
+    cout << '\n';
+  }
+  cout << endl;
 }
 
-template <typename T, size_t N, size_t M>
-void print2DL(T(&arr2D)[N][M]) {
-    for (size_t r = 0; r < N; ++r) {
-        for (size_t c = 0; c < M; ++c) {
-            cout << arr2D[r][c] << " ";
-        }
-        cout << '\n';
+template <typename T> void printSquare(T **arrSqr, size_t N) {
+  for (size_t row = 0; row < N; ++row) {
+    for (size_t column = 0; column < N; ++column) {
+      cout << arrSqr[row][column] << " ";
     }
-    cout << endl;
+    cout << '\n';
+  }
+  cout << endl;
 }
 
-template <typename T>
-void print2DA(T **arrSqr, size_t N, size_t M) {
-    for (size_t row = 0; row < N; ++row) {
-        for (size_t column = 0; column < M; ++column) {
-            cout << arrSqr[row][column] << " ";
-        }
-        cout << '\n';
-    }
-    cout << endl;
-}
+// const int INF = 1e9;
+// int n, m, k;
+// int *r, *c;
+// int **dp;
 
-template <typename T>
-void printSquare(T **arrSqr, size_t N) {
-    for (size_t row = 0; row < N; ++row) {
-        for (size_t column = 0; column < N; ++column) {
-            cout << arrSqr[row][column] << " ";
-        }
-        cout << '\n';
-    }
-    cout << endl;
-}
-
-
-//const int INF = 1e9;
-//int n, m, k;
-//int *r, *c;
-//int **dp;
-
-//struct Point {
+// struct Point {
 //    ll f, s;
 //    Point() {}
 //    Point(ll first, ll second) {
@@ -87,18 +79,18 @@ void printSquare(T **arrSqr, size_t N) {
 
 //};
 
-//ostream& operator << (ostream &o, const Point& p) {
+// ostream& operator << (ostream &o, const Point& p) {
 //    o << "(" << p.f << "," << p.s << ")";
 //    return o;
 //}
 
-//bool pointCmp(Point const& p1, Point const& p2) {
+// bool pointCmp(Point const& p1, Point const& p2) {
 //    return (p1.f < p2.f);
 //}
 
-//Point* P;
+// Point* P;
 
-//ll take_photos(int n, int m, int k, int *r, int *c) {
+// ll take_photos(int n, int m, int k, int *r, int *c) {
 //    FOR(i, n) {
 //        P[i] = Point(r[i], c[i]);
 //    }
@@ -117,7 +109,8 @@ void printSquare(T **arrSqr, size_t N) {
 //    FORN(i, 1, n+1) {
 //        FORN(j, 1, k+1) {
 //            FOR(t, i) {
-//                dp[i][j] = min(dp[i][j], (int)(dp[t][j-1] + pow((P[i-1].f - P[i-1].s + 1), 2)));
+//                dp[i][j] = min(dp[i][j], (int)(dp[t][j-1] + pow((P[i-1].f -
+//                P[i-1].s + 1), 2)));
 //            }
 //        }
 //    }
@@ -125,7 +118,7 @@ void printSquare(T **arrSqr, size_t N) {
 //    return dp[n][k];
 //}
 
-//void Solve() {
+// void Solve() {
 //    scan(n);
 //    scan(m);
 //    scan(k);
@@ -145,57 +138,50 @@ void printSquare(T **arrSqr, size_t N) {
 //}
 
 void Solve() {
-    int H, D, E;
-    int A[5], S[5], T[1000], L[1000], X[1000];
-    int shift[1000];
-    int battleTime = 0;
-    scan(H);
-    scan(D);
-    scan(E);
-    H *= 100;
-    FOR(i, D) {
-        scan(A[i]);
-        scan(S[i]);
-        S[i] *= 100;
+  const int INF = 0x3f3f3f3f;
+  const int MAXN = 200000;
+  int N, M, K;
+  int W = INF;
+  int a, b;
+  scan(N);
+  scan(M);
+  scan(K);
+  int potats[MAXN];
+  FOR(i, N) { potats[i] = M; }
+  FOR(i, M) {
+    scan(a);
+    scan(b);
+    FORN(j, a - 1, b) { potats[j]--; }
+  }
+  int sum = 0;
+  int lo = 0;
+  FOR(hi, N) {
+    sum += potats[hi];
+    if (sum >= K) {
+      //            cout << hi << " " << lo << ": " << sum << " ";
+      while (sum - potats[lo] >= K) {
+        sum -= potats[lo];
+        lo++;
+        //                cout << sum << " ";
+      }
+      W = min(W, hi - lo + 1);
+      //            print(W);
     }
-    FOR(i, E) {
-        scan(T[i]);
-        scan(L[i]);
-        scan(X[i]);
-        X[i] *= 100;
-        shift[T[i]] += X[i];
-        shift[T[i] + L[i]] -= X[i];
-        if (battleTime < T[i] + L[i]) {
-            battleTime = T[i] + L[i];
-        }
-    }
-    int curDmg = 0;
-    FOR(t, battleTime) {
-        curDmg += shift[t];
-        int newDmg = curDmg;
-        FOR(i, D) {
-            newDmg = min(newDmg, (curDmg - S[i])/100 * (100 - A[i]));
-        }
-        if (newDmg > 0) {
-            H -= newDmg;
-        }
-    }
-    if (H > 0) {
-        printf("%.2f", (double)H/100);
-    } else {
-        printf("DO A BARREL ROLL!");
-    }
+  }
+  if (W == INF)
+    W = -1;
+  printf("%d", W);
 }
 
 int main() {
-    freopen("../DMOJ/input.txt", "r", stdin);
+  freopen("../DMOJ/input.txt", "r", stdin);
 
-    time_point<steady_clock> start = steady_clock::now();
+  time_point<steady_clock> start = steady_clock::now();
 
-    Solve();
+  Solve();
 
-    time_point<steady_clock> end = steady_clock::now();
-    auto diff = duration_cast<milliseconds>(end - start);
-    cout << "\nTime: " << diff.count() << " ms" << endl;
-    return 0;
+  time_point<steady_clock> end = steady_clock::now();
+  auto diff = duration_cast<milliseconds>(end - start);
+  cout << "\nTime: " << diff.count() << " ms" << endl;
+  return 0;
 }

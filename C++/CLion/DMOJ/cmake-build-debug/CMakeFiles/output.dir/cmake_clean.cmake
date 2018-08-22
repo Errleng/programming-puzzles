660ca6eee320427d825d8ac482a01324837ee1de
ELEVATE <dullbland@gmail.com>
2018-08-22 05:35:37 -0400

    Rearranged files and projects

---
+++ C++/CLion/DMOJ/cmake-build-debug/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/output/main.cpp.obj"
+  "output.pdb"
+  "output.exe"
+  "output.exe.manifest"
+  "liboutput.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

09158653e5ad81faba51e90a6ddeab95c0ade598
halfDone <dullbland@gmail.com>
2018-09-12 22:40:16 -0400

    cco10p1 completed

--- C++/CLion/DMOJ/cmake-build-debug/cmake_install.cmake
+++ C++/CLion/DMOJ/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Documents/SourceTree/Challenges/C++/CLion/DMOJ
+# Install script for directory: /home/pilot/GitRepos/challenges/C++/CLion/DMOJ
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/DMOJ")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -40,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Documents/SourceTree/Challenges/C++/CLion/DMOJ/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/pilot/GitRepos/challenges/C++/CLion/DMOJ/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

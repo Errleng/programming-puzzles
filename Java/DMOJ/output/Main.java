e7a414e4448cac2b2ab0ae1b5e132304f7ca9dc5
halfDone <dullbland@gmail.com>
2018-09-14 08:52:05 -0400

    Doing ccc15s3 using AVL Tree in Java

--- Java/DMOJ/output/Main.java
+++ Java/DMOJ/output/Main.java
@@ -2,18 +2,10 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.Set;
-import java.io.IOException;
-import java.util.HashMap;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.math.BigInteger;
+import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.InputStream;
 
 /**
@@ -26,180 +18,208 @@ public class Main {
         OutputStream outputStream = System.out;
         FastScanner in = new FastScanner(inputStream);
         PrintWriter out = new PrintWriter(outputStream);
-        fibonacci2 solver = new fibonacci2();
+        ccc15s3 solver = new ccc15s3();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class fibonacci2 {
-        public static BigInteger TWO = new BigInteger("2");
-        public static BigInteger THREE = new BigInteger("3");
-        public static BigInteger FIVE = new BigInteger("5");
-
-        public static BigInteger lcm(BigInteger a, BigInteger b) {
-            BigInteger temp = a.gcd(b);
-            if (temp.equals(BigInteger.ZERO)) {
-                return BigInteger.ZERO;
-            } else {
-                return (a.divide(temp).multiply(b));
-            }
-        }
-
-        public static BigInteger sqrt(BigInteger x) {
-            BigInteger div = BigInteger.ZERO.setBit(x.bitLength() / 2);
-            BigInteger div2 = div;
-            // Loop until we hit the same value twice in a row, or wind
-            // up alternating.
-            for (; ; ) {
-                BigInteger y = div.add(x.divide(div)).shiftRight(1);
-                if (y.equals(div) || y.equals(div2))
-                    return y;
-                div2 = div;
-                div = y;
-            }
-        }
-
-        public static ArrayList<BigInteger> divisors(BigInteger N) {
-            ArrayList<BigInteger> res = new ArrayList<BigInteger>();
-            for (BigInteger i = BigInteger.ONE; i.compareTo(sqrt(N)) <= 0; i = i.add(BigInteger.ONE)) {
-                if (N.mod(i).equals(BigInteger.ZERO)) {
-                    if (N.divide(i).equals(i)) {
-                        res.add(i);
-                    } else {
-                        res.add(i);
-                        res.add(N.divide(i));
-                    }
-                }
-            }
-            return res;
-        }
-
-        public static ArrayList<BigExp> prime_factorize(BigInteger N) {
-            HashMap<BigInteger, Integer> exp = new HashMap<>();
-            while (N.mod(TWO).equals(BigInteger.ZERO)) {
-                Integer val = exp.get(TWO);
-                if (val != null) {
-                    exp.put(TWO, val + 1);
-                } else {
-                    exp.put(TWO, 1);
-                }
-                N = N.divide(TWO);
-            }
-            for (BigInteger i = THREE; i.compareTo(sqrt(N)) <= 0; i = i.add(TWO)) {
-                while (N.mod(i).equals(BigInteger.ZERO)) {
-                    Integer val = exp.get(i);
-                    if (val != null) {
-                        exp.put(i, val + 1);
-                    } else {
-                        exp.put(i, 1);
-                    }
-                    N = N.divide(i);
-                }
-            }
-            if (N.compareTo(TWO) > 0) {
-                Integer val = exp.get(N);
-                if (val != null) {
-                    exp.put(N, val + 1);
-                } else {
-                    exp.put(N, 1);
-                }
-            }
-            ArrayList<BigExp> res = new ArrayList<BigExp>();
-            Iterator it = exp.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry) it.next();
-                res.add(new BigExp((BigInteger) pair.getKey(), (Integer) pair.getValue()));
-                it.remove();
-            }
-            return res;
-        }
-
-        public static BigInteger prime_power_divisor(BigInteger p, int k) {
-            if (k == 0)
-                return BigInteger.ONE;
-            else if (p.equals(FIVE))
-                return (p.pow(k - 1)).multiply(FIVE);
-            BigInteger e = (p.mod(FIVE).equals(TWO) || p.mod(FIVE).equals(THREE)) ? BigInteger.ONE : new BigInteger("-1");
-            for (BigInteger i : divisors(p.add(e))) {
-                if (fib_mod(i, p).equals(BigInteger.ZERO)) {
-                    return i.multiply(p.pow(k - 1));
-                }
-            }
-            throw new RuntimeException("Prime power divisor not found. p = " + p.toString() + " k = " + k);
-        }
-
-        public static BigInteger pisano_period(BigInteger N) {
-            if (N.compareTo(BigInteger.ZERO) < 1)
-                return BigInteger.ZERO;
-            else if (N.equals(BigInteger.ONE))
-                return BigInteger.ONE;
-            BigInteger d = BigInteger.ONE;
-            ArrayList<BigExp> pf = prime_factorize(N);
-            for (BigExp i : pf) {
-                d = lcm(d, prime_power_divisor(i.f, i.s));
-//            System.out.println("(" + i.f.toString() + "," + i.s + ") " + prime_power_divisor(i.f, i.s).toString() + ": " + d.toString());
-            }
-            BigInteger t = BigInteger.ZERO;
-            for (int i = 0; i < 3; ++i) {
-                t = d.shiftLeft(i);
-                if (fib_mod(t, N).equals(BigInteger.ZERO) && fib_mod(t.add(BigInteger.ONE), N).equals(BigInteger.ONE)) {
-                    return t;
-                }
-            }
-            throw new java.lang.RuntimeException("CONJECTURE DISPROVEN WITH N = " + N.toString() + ", d = " + d.toString() + ", t = " + t.toString());
-        }
-
-        public static BigInteger fib_mod(BigInteger N, BigInteger m) {
-            BigInteger f = BigInteger.ZERO;
-            BigInteger g = BigInteger.ONE;
-            BigInteger a = BigInteger.ZERO;
-            BigInteger b = BigInteger.ONE;
-            for (int i = 0; i <= N.bitLength(); i++) {
-                if (N.testBit(i)) {
-                    BigInteger t = f;
-                    f = ((f.multiply(a)).add(g.multiply(b))).mod(m);
-                    g = ((t.multiply(b)).add(g.multiply(a.add(b)))).mod(m);
-                }
-                BigInteger t = a;
-                a = ((a.multiply(a)).add(b.multiply(b))).mod(m);
-                b = ((t.multiply(b)).add(b.multiply(t.add(b)))).mod(m);
-            }
-            return f;
-        }
-
+    static class ccc15s3 {
         public void solve(int testNumber, FastScanner in, PrintWriter out) {
-            BigInteger N = new BigInteger(in.next());
-            BigInteger M = new BigInteger(in.next());
-            out.print(fib_mod(N, M));
-            out.print(" ");
-            out.print(pisano_period(M));
-
-            int LIMIT = 11;
-            for (int i = 2; i <= LIMIT; i++) {
-                for (int j = 2; j <= LIMIT; j++) {
-                    BigInteger test = BigInteger.valueOf(i).pow(j);
-                    System.out.print(pisano_period(test).toString() + " ");
-//                System.out.println(test.toString() + ": " + pisano_period(test).toString());
-                }
+            int G = in.nextInt();
+            int P = in.nextInt();
+            boolean[] docked = new boolean[G + 1];
+            AVLTree tree = new AVLTree();
+            for (int i = 1; i <= G; ++i) {
+                tree.Insert(-i);
             }
-            System.out.println();
-//        N = new BigInteger("31381059609");
-//        BigInteger D = new BigInteger("41841412812");
-//        for (int i = 0; i < 3; i++) {
-//            BigInteger T = D.shiftLeft(i);
-//            System.out.println(T.toString() + ": " + fib_mod(T, N).toString() + " " + fib_mod(T.add(BigInteger.ONE), N).toString());
-//        }
+            for (int i = 1; i <= P; ++i) {
+                int g = in.nextInt();
+                if (tree.GetNode(-g) != null) {
+                    out.println(i);
+                    return;
+                }
+                Node get = tree.GetNode(-g);
+                if (get != null) {
+                    tree.Delete(get.val);
+                }
+//            boolean fail = true;
+//            for (int j = g; j > 0; --j) {
+//                if (!docked[j]) {
+//                    fail = false;
+//                    docked[j] = true;
+//                    ++planes;
+//                    break;
+//                }
+//            }
+//            if (fail) {
+//                break;
+//            }
+            }
+            out.println(P);
         }
 
     }
 
-    static class BigExp {
-        BigInteger f;
-        int s;
+    static class Node {
+        public int val;
+        public int height;
+        public Node left;
+        public Node right;
 
-        BigExp(BigInteger a, int b) {
-            this.f = a;
-            this.s = b;
+        public Node(int data) {
+            val = data;
+            height = 1;
+        }
+
+    }
+
+    static class AVLTree {
+        Node root;
+
+        int max(int a, int b) {
+            return (a > b) ? a : b;
+        }
+
+        int GetHeight(Node n) {
+            if (n == null)
+                return 0;
+            return n.height;
+        }
+
+        int GetHeightDifference(Node n) {
+            if (n == null)
+                return 0;
+            return (GetHeight(n.left) - GetHeight(n.right));
+        }
+
+        Node RightRotation(Node y) {
+            Node x = y.left; // Shift left child to be new root
+            Node temp = x.right; // Save x right child to give to y
+            x.right = y; // Set y to x right child
+            y.left = temp; // Give x right child to y as left child
+            y.height = max(GetHeight(y.left), GetHeight(y.right)) + 1;
+            x.height = max(GetHeight(x.left), GetHeight(x.right)) + 1;
+            return x; // New root n of the subtree
+        }
+
+        Node LeftRotation(Node x) {
+            Node y = x.right;
+            Node temp = y.left;
+            y.left = x;
+            x.right = temp;
+            x.height = max(GetHeight(x.left), GetHeight(x.right)) + 1;
+            y.height = max(GetHeight(y.left), GetHeight(y.right)) + 1;
+            return y;
+        }
+
+        Node GetNode(int val) {
+            return SearchForNode(root, val);
+        }
+
+        Node SearchForNode(Node n, int val) {
+            if (n == null) {
+                return null;
+            }
+            if (n.val < val) {
+                return SearchForNode(n.left, val);
+            } else if (n.val > val) {
+                return SearchForNode(n.right, val);
+            } else {
+                return n;
+            }
+        }
+
+        void Insert(int val) {
+            root = InsertNode(root, val);
+        }
+
+        Node InsertNode(Node n, int val) {
+            if (n == null)
+                return (new Node(val));
+
+            if (val < n.val)
+                n.left = InsertNode(n.left, val);
+            else if (val > n.val)
+                n.right = InsertNode(n.right, val);
+            else
+                return n;
+
+            n.height = max(GetHeight(n.left), GetHeight(n.right)) + 1;
+
+            int balance = GetHeightDifference(n);
+
+            if (balance > 1 && val < n.left.val)
+                return RightRotation(n);
+            if (balance > 1 && val > n.left.val) {
+                n.left = LeftRotation(n.left);
+                return RightRotation(n);
+            }
+            if (balance < -1 && val > n.right.val)
+                return LeftRotation(n);
+            if (balance < -1 && val < n.right.val) {
+                n.right = RightRotation(n.right);
+                return LeftRotation(n);
+            }
+            return n;
+        }
+
+        Node MinNode(Node n) {
+            Node cur = n;
+            while (cur.left != null)
+                cur = cur.left;
+            return cur;
+        }
+
+        void Delete(int val) {
+            this.DeleteNode(root, val);
+        }
+
+        Node DeleteNode(Node n, int val) {
+            if (n == null) {
+                return n;
+            }
+            if (val < n.val) {
+                n.left = DeleteNode(n.left, val);
+            } else if (val > n.val) {
+                n.right = DeleteNode(n.right, val);
+            } else {
+                if ((n.left == null) || (n.right == null)) {
+                    Node t = null;
+                    if (t == n.left) {
+                        t = n.right;
+                    } else {
+                        t = n.left;
+                    }
+                    if (t == null) {
+                        t = n;
+                        n = null;
+                    } else {
+                        n = t;
+                    }
+                } else {
+                    Node t = MinNode(n.right);
+                    n.val = t.val;
+                    n.right = DeleteNode(n.right, t.val);
+                }
+            }
+            if (n == null) {
+                return n;
+            }
+            n.height = max(GetHeight(n.left), GetHeight(n.right));
+            int balance = GetHeightDifference(n);
+            if (balance > 1 && GetHeightDifference(root.left) >= 0)
+                return RightRotation(root);
+            if (balance > 1 && GetHeightDifference(root.left) < 0) {
+                root.left = LeftRotation(root.left);
+                return RightRotation(root);
+            }
+            if (balance < -1 && GetHeightDifference(root.right) <= 0)
+                return LeftRotation(root);
+            if (balance < -1 && GetHeightDifference(root.right) > 0) {
+                root.right = RightRotation(root.right);
+                return LeftRotation(root);
+            }
+            return root;
         }
 
     }
@@ -222,6 +242,10 @@ public class Main {
                 }
             }
             return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
         }
 
     }
